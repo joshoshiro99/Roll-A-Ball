d598c7e066b2a758cd4e40d4c8624ca4ec6929d0
Jojo <53246097+joshoshiro99@users.noreply.github.com>
2021-03-03 02:11:06 -0500

    Update to 1.75
    removed jump features. imported ramp and finalized boost setup. downforce added to keep ball on ramp asmuchaspossible

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,18 +6,17 @@ using TMPro;
 
 public class playerController : MonoBehaviour
 {
+    public float downforce = 5;
+    public float sideforce = 5;
     public float speed = 0;
     public float boost = 10;
-    public float jumpForce = 10;
     public TextMeshProUGUI countText;
     public GameObject winText;
-    public GameObject door;
 
     private Rigidbody rb;
     private int count;
     private float movementx;
     private float movementy;
-    private int jumps = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,34 +27,24 @@ public class playerController : MonoBehaviour
         winText.SetActive(false);
     }
 
-    void OnMove(InputValue movementValue)
+    public void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementx = movementVector.x;
         movementy = movementVector.y;
     }
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        float jumpInput = context.ReadValue<float>();
-    }
 
     void SetCountText()
     {
-        countText.text = "Jumps: " + jumps.ToString();
-        if(count>=12)
-        {
-            winText.SetActive(true);
-            door.SetActive(false);
-        }
+        countText.text = "SpeedMuliplier: x" + count.ToString();
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementx, 0.0f, movementy);
+        Vector3 movement = new Vector3(movementx*speed, downforce, movementy*speed);
 
-        rb.AddForce(movement*speed);
+        rb.AddForce(movement);
 
-        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -70,9 +59,16 @@ public class playerController : MonoBehaviour
         if(other.gameObject.CompareTag("jumps"))
         {
             other.gameObject.SetActive(false);
-            jumps++;
             count++;
             SetCountText();
+        }
+        if(other.gameObject.CompareTag("speedupdone"))
+        {
+            downforce = 0;
+        }
+        if (other.gameObject.CompareTag("final"))
+        {
+            downforce = 0;
         }
     }
 
